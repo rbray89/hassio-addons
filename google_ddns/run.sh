4563d5385ed0be8b6623ef433545e273895e3509
Ryan Bray <rbray89@gmail.com>
2019-02-13 04:19:08 -0700

    Adding A simple google DDNS client

---
+++ google_ddns/run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+CONFIG_PATH=/data/options.json
+
+USERNAME=$(jq --raw-output ".username" $CONFIG_PATH)
+PASSWORD=$(jq --raw-output ".password" $CONFIG_PATH)
+DOMAIN=$(jq --raw-output ".domain" $CONFIG_PATH)
+IPFLTR=$(jq --raw-output ".ipfltr" $CONFIG_PATH)
+IP_LAST=""
+
+while true; do
+    IP_LATEST=$(eval $IPFLTR)
+    if [ "$IP_LATEST" != "$IP_LAST" ]; then
+       wget "https://$USERNAME:$PASSWORD@domains.google.com/nic/update?hostname=$DOMAIN&myip=$IP_LATEST" -O -
+    fi
+    IP_LAST=$IP_LATEST
+    sleep 5s
+done
