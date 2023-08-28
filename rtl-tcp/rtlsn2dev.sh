8f2783f8a7608104e9e8aa2b18fefa502b5a76f2
root <root@a0d7b954-vscode.local.hass.io>
2023-08-27 23:50:07 -0600

    Fix rtlsn2dev.sh

--- rtl-tcp/rtlsn2dev.sh
+++ rtl-tcp/rtlsn2dev.sh
@@ -3,17 +3,5 @@
 # returns: device ID
 if [ $1 ]
 then
-  rtl_num_devices=$(rtl_eeprom 2>&1 >/dev/null | grep "Found [0-9][0-9]*" | sed -E 's/.*([0-9]+).*/\1/')
-  if [ $rtl_num_devices ]
-  then
-    for i in $(seq 1 $rtl_num_devices);
-    do
-      rtl_device=$((i-1))
-      rtl_serial=$(rtl_eeprom -d$rtl_device 2>&1 >/dev/null | grep "Serial number\:" | sed -E 's/Serial number:[[:blank:]]+//')
-      if [ "$1" == "$rtl_serial" ]
-      then
-        echo $rtl_device
-      fi
-    done
-  fi
+  rtl_tcp -d 9999 |& sed -E -n "s/([0-9]+):\s*.+SN: $1/\1/p"
 fi
